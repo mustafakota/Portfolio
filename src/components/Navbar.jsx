066cd4ec3b708acff739239c4f288a2de08c6258
Mustafa <mustafakota88@gmail.com>
2026-05-09 00:09:48 +0530

    mann

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -157,7 +157,6 @@ export default function Navbar() {
             borderRadius: '999px',
             color: 'white',
             cursor: 'pointer',
-            display: 'none',
             zIndex: 101
           }}
         >
